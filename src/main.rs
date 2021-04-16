1798f9940da1491577aa102fc9218ce29b534095
Matt Wilkinson <mattwilki17@gmail.com>
2021-04-15 23:53:16 -0400

    7.2.4

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -19,11 +20,19 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to Postgres");
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
 
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await?;
+
+    Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,6 +6,7 @@ use tokio;
 use uuid::Uuid;
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -55,7 +56,13 @@ async fn spawn_app() -> TestApp {
     config.database.database_name = Uuid::new_v4().to_string();
     let db_pool = configure_database(&config.database).await;
 
-    let server = run(listener, db_pool.clone()).expect("Failed to run server");
+    let sender_email = config
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(config.email_client.base_url, sender_email);
+
+    let server = run(listener, db_pool.clone(), email_client).expect("Failed to run server");
 
     let _ = tokio::spawn(server);
 
